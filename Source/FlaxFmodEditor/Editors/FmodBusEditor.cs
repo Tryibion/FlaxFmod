1c4cdc6044b717746ecc22b35df49ae9ba79e9a7
Chandler Cox <mr.chandlercox@gmail.com>
2025-08-22 22:22:01 -0500

    Add FmodBus editor will warning not to edit the asset.

---
+++ Source/FlaxFmodEditor/Editors/FmodBusEditor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEditor.CustomEditors;
+using FlaxEditor.CustomEditors.Editors;
+using FlaxEngine;
+
+namespace FlaxFmod.Editor;
+
+/// <summary>
+/// FmodBusEditor class.
+/// </summary>
+[CustomEditor(typeof(FmodBus))]
+public class FmodBusEditor : GenericEditor
+{
+    /// <inheritdoc />
+    public override void Initialize(LayoutElementsContainer layout)
+    {
+        var labelElement = layout.Label("This Fmod Bus is auto generated. Please do not edit it manually.");
+        labelElement.Label.HorizontalAlignment = TextAlignment.Center;
+        base.Initialize(layout);
+    }
+}
