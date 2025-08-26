94ff54315921586a3e0aae4e1ef036fc450ca83d
Chandler Cox <mr.chandlercox@gmail.com>
2025-08-25 21:24:52 -0500

    Fix linux linker issues by removing so versioning.

--- Source/FlaxFmod/FlaxFmod.Build.cs
+++ Source/FlaxFmod/FlaxFmod.Build.cs
@@ -20,9 +20,9 @@ public class FlaxFmod : GameModule
         options.ScriptingAPI.IgnoreMissingDocumentationWarnings = true;
         
         var fmodPath = Path.Combine(FolderPath, "..", "Fmod");
-        var coreIncludePath = Path.Combine(FolderPath, "..", "Fmod", "Include", "core");
+        var coreIncludePath = Path.Combine(fmodPath, "Include", "core");
         options.CompileEnv.IncludePaths.Add(coreIncludePath);
-        var studioIncludePath = Path.Combine(FolderPath, "..", "Fmod", "Include", "studio");
+        var studioIncludePath = Path.Combine(fmodPath, "Include", "studio");
         options.CompileEnv.IncludePaths.Add(studioIncludePath);
 
         switch (options.Platform.Target)
@@ -45,17 +45,17 @@ public class FlaxFmod : GameModule
             case TargetPlatform.Linux:
                 // FMod Core Library
                 var linCoreLibPath = Path.Combine(fmodPath, "Linux", "core");
-                options.DependencyFiles.Add(Path.Combine(linCoreLibPath, "libfmod.so.14.8"));
-                options.DependencyFiles.Add(Path.Combine(linCoreLibPath, "libfmodL.so.14.8"));
-                options.Libraries.Add(Path.Combine(linCoreLibPath, "libfmod.so.14.8"));
-                options.Libraries.Add(Path.Combine(linCoreLibPath, "libfmodL.so.14.8"));
+                options.DependencyFiles.Add(Path.Combine(linCoreLibPath, "libfmod.so"));
+                options.DependencyFiles.Add(Path.Combine(linCoreLibPath, "libfmodL.so"));
+                options.Libraries.Add(Path.Combine(linCoreLibPath, "libfmod.so"));
+                options.Libraries.Add(Path.Combine(linCoreLibPath, "libfmodL.so"));
 
                 // FMod Studio Library
                 var linStudioLibPath = Path.Combine(fmodPath, "Linux", "studio");
-                options.DependencyFiles.Add(Path.Combine(linStudioLibPath, "libfmodstudio.so.14.8"));
-                options.DependencyFiles.Add(Path.Combine(linStudioLibPath, "libfmodstudioL.so.14.8"));
-                options.Libraries.Add(Path.Combine(linStudioLibPath, "libfmodstudio.so.14.8"));
-                options.Libraries.Add(Path.Combine(linStudioLibPath, "libfmodstudioL.so.14.8"));
+                options.DependencyFiles.Add(Path.Combine(linStudioLibPath, "libfmodstudio.so"));
+                options.DependencyFiles.Add(Path.Combine(linStudioLibPath, "libfmodstudioL.so"));
+                options.Libraries.Add(Path.Combine(linStudioLibPath, "libfmodstudio.so"));
+                options.Libraries.Add(Path.Combine(linStudioLibPath, "libfmodstudioL.so"));
                 break;
             default:
                 break;
