4341fe83aa7fb9bd300688408261757efd3dfcfd
Chandler Cox <mr.chandlercox@gmail.com>
2025-08-28 20:49:11 -0500

    Add FmodBankEditor with generated asset warning.

---
+++ Source/FlaxFmodEditor/Editors/FmodBankEditor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEditor.CustomEditors;
+using FlaxEditor.CustomEditors.Editors;
+using FlaxEngine;
+
+namespace FlaxFmod.Editor;
+
+/// <summary>
+/// FmodEventEditor class.
+/// </summary>
+[CustomEditor(typeof(FmodBank))]
+public class FmodBankEditor : GenericEditor
+{
+    /// <inheritdoc />
+    public override void Initialize(LayoutElementsContainer layout)
+    {
+        var labelElement = layout.Label("This Fmod Bank is auto generated. Please do not edit it manually.");
+        labelElement.Label.HorizontalAlignment = TextAlignment.Center;
+        base.Initialize(layout);
+    }
+}
