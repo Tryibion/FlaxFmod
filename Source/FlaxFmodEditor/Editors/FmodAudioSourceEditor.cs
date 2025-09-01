d3cdcb7a7d9519d45f72fa57e66a597271d33b5b
Chandler Cox <mr.chandlercox@gmail.com>
2025-08-31 19:24:36 -0500

    Add on par with AudioSource playback editor for FmodAudioSource.

---
+++ Source/FlaxFmodEditor/Editors/FmodAudioSourceEditor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using FlaxEditor.CustomEditors;
+using FlaxEditor.CustomEditors.Dedicated;
+using FlaxEngine;
+using FlaxEngine.GUI;
+
+namespace FlaxFmod.Editor;
+
+[CustomEditor(typeof(FmodAudioSource)), DefaultEditor]
+public class FmodAudioSourceEditor : ActorEditor
+{
+    private Label _infoLabel;
+
+    /// <inheritdoc />
+    public override void Initialize(LayoutElementsContainer layout)
+    {
+        base.Initialize(layout);
+
+        // Show playback options during simulation
+        if (FlaxEditor.Editor.IsPlayMode)
+        {
+            var playbackGroup = layout.Group("Playback");
+            playbackGroup.Panel.Open();
+
+            _infoLabel = playbackGroup.Label(string.Empty).Label;
+            _infoLabel.AutoHeight = true;
+
+            var grid = playbackGroup.UniformGrid();
+            var gridControl = grid.CustomControl;
+            gridControl.ClipChildren = false;
+            gridControl.Height = Button.DefaultHeight;
+            gridControl.SlotsHorizontally = 3;
+            gridControl.SlotsVertically = 1;
+            grid.Button("Play").Button.Clicked += () => Foreach(x => x.Play());
+            grid.Button("Pause").Button.Clicked += () => Foreach(x => x.Pause());
+            grid.Button("Stop").Button.Clicked += () => Foreach(x => x.Stop());
+        }
+    }
+
+    /// <inheritdoc />
+    public override void Refresh()
+    {
+        base.Refresh();
+
+        if (_infoLabel != null)
+        {
+            var text = string.Empty;
+            foreach (var value in Values)
+            {
+                if (value is FmodAudioSource audioSource && audioSource.Event)
+                    text += $"Time: {audioSource.GetEventPosition():##0.0}s / {audioSource.GetEventLength():##0.0}s\n";
+            }
+            _infoLabel.Text = text;
+        }
+    }
+
+    private void Foreach(Action<FmodAudioSource> func)
+    {
+        foreach (var value in Values)
+        {
+            if (value is FmodAudioSource audioSource)
+                func(audioSource);
+        }
+    }
+}
